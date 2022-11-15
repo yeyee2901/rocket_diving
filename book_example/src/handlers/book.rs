d62b3387c9f42cf2e3190235c890ab03718d0f33
Gabriel Sebastian <gabriel777sh@gmail.com>
2022-11-15 12:55:41 +0700

    edit: edit via reset --soft

--- book_example/src/handlers/book.rs
+++ book_example/src/handlers/book.rs
@@ -1,19 +1,116 @@
-use rocket::{get, serde::json::Json};
+use rocket::serde::json::Json;
+use rocket::{get, post};
 use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+use validator::{Validate, ValidationError};
 
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Book {
-    title: String,
-    publisher: String,
-    year: u32,
+/// Book Models are defined here
+pub mod model {
+    use super::*;
+
+    #[derive(Serialize, Deserialize, ToSchema)]
+    pub struct Book {
+        /// title of the book
+        pub title: String,
+
+        /// publisher of the book
+        pub publisher: String,
+
+        /// publication year
+        pub year: u32,
+    }
+
+    #[derive(
+        Serialize, Deserialize, ToSchema, Validate,
+    )]
+    #[validate(schema(function = "validate_req_create_book"))]
+    pub struct ReqCreateBook {
+        /// new book title
+        #[schema(example = "The Rust Book")]
+        pub title: String,
+    }
+
+    #[derive(Serialize, ToSchema)]
+    #[serde(rename_all = "camelCase")]
+    pub struct RespCreateBook {
+        /// response code
+        pub response_code: String,
+
+        /// response message, explaining operation error / success details
+        pub response_message: String,
+
+        /// data, the data scheme should match the context of the endpoint
+        pub data: Book,
+    }
+
+
+    /// validate book creation on "POST /api/book"
+    pub fn validate_req_create_book(
+        req: &ReqCreateBook,
+    ) -> Result<(), ValidationError> {
+        match req.title.is_empty() {
+            false => Ok(()),
+            true => Err(ValidationError::new(
+                "Title must be non empty string",
+            )),
+        }
+    }
 }
 
+/// Get a book
+#[utoipa::path(
+    context_path = "/api",
+    responses(
+        (status = 200, description = "get single book", body = [Book]),
+    )
+)]
 #[get("/book")]
-pub fn get_book() -> Json<Book> {
-    Json(Book {
-        title: "Lorem ipsum".to_string(),
-        publisher: "set dolor".to_string(),
+pub fn get_book() -> Json<model::Book> {
+    Json(model::Book {
+        title: "this is edited via rebasing".to_string(),
+        publisher: "this is edited via reset --soft".to_string(),
         year: 2022,
     })
+}
+
+/// Insert a new book
+#[utoipa::path(
+    context_path = "/api",
+    request_body = ReqCreateBook,
+    responses(
+        (status = 200, description = "Success", body = [RespCreateBook]),
+        (status = 400, description = "Bad Request", body = [RespCreateBook]),
+    )
+)
+]
+#[post("/book", data = "<new_book>")]
+pub async fn create_book(
+    new_book: Json<model::ReqCreateBook>,
+) -> Json<model::RespCreateBook> {
+    let req = new_book.into_inner();
+
+    match req.validate() {
+        Ok(_) => {
+            return Json(model::RespCreateBook {
+                response_code: "200".to_string(),
+                response_message: "success".to_string(),
+                data: model::Book {
+                    title: req.title,
+                    publisher: "The JSON validator".to_string(),
+                    year: 2022,
+                },
+            })
+        }
+        Err(err) => {
+            return Json(model::RespCreateBook {
+                response_code: "400".to_string(),
+                response_message: format!("Bad Request. {}", err).to_string(),
+                data: model::Book {
+                    title: req.title,
+                    publisher: "The JSON validator".to_string(),
+                    year: 2022,
+                },
+            })
+        }
+    }
 }
--- book_example/src/lib.rs
+++ book_example/src/lib.rs
@@ -4,11 +4,47 @@ mod handlers;
 // external crates
 use handlers::book;
 use rocket::{routes, Build, Rocket};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+/// Swagger documentations are declared here using proc-macro
+#[derive(OpenApi)]
+#[openapi(
+    paths(
+        book::get_book,
+        book::create_book,
+    ),
+    components(
+        schemas(
+            book::model::Book,
+            book::model::ReqCreateBook,
+            book::model::RespCreateBook
+        )
+    ),
+    tags(
+        (name = "book", description = "Book related requests")
+    )
+)]
+struct ApiDoc;
 
 #[rocket::launch]
 pub async fn server() -> Rocket<Build> {
-    rocket::custom(config::load_config()).mount(
-        "/api",
-        routes![book::get_book],
-    )
+    // load config file
+    let app_cfg = config::load_config();
+
+    // build the app
+    rocket::custom(app_cfg)
+        // mount swagger UI
+        .mount(
+            "/",
+            SwaggerUi::new("/swagger/<_..>").url(
+                "/doc/openapi.json",
+                ApiDoc::openapi(),
+            ),
+        )
+        // populate the endpoints
+        .mount(
+            "/api",
+            routes![book::get_book, book::create_book],
+        )
 }
